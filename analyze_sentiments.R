4a239a1ea2f56e1001113d7a241e0ee9ed50b027
elkalytics <napoleonic_bores@proton.me>
2023-04-19 22:07:47 -0400

    Adjusted the scoring and added more pre-processing
    Adjusted scoring to have thresholds set to each individual method. I also added the stop words removal parameter as an option.

--- analyze_sentiments.R
+++ analyze_sentiments.R
@@ -1,11 +1,18 @@
 #' Analyze sentiments for text columns in a data frame
 #'
-#' @param data A data frame with text columns to analyze
-#' @param text_columns A character vector of the names of the text columns to analyze
-#' @param methods A character vector of sentiment analysis methods to use. Valid options are "syuzhet", "afinn", and "bing"
-#' @param preprocess A logical indicating whether or not to preprocess the text by converting to lowercase, removing punctuation, and squishing whitespace
+#' This function takes a data frame, a vector of text column names, and a vector of sentiment analysis methods, and
+#' returns a new data frame with sentiment scores and labels for each method and overall majority sentiment labels
+#' for each text column per row.
 #'
-#' @return A data frame with sentiment analysis scores and labels for each text column and row
+#' @param data A data frame containing the text columns to be analyzed.
+#' @param text_columns A character vector specifying the names of the text columns in the data frame to analyze.
+#' @param methods A character vector specifying the sentiment analysis methods to use. Default is c("syuzhet", "afinn", "bing").
+#' @param preprocess A logical flag indicating whether to preprocess the text data by converting it to lowercase,
+#'                   removing special characters, and trimming white spaces. Default is FALSE.
+#' @param remove_stopwords A logical flag indicating whether to remove stop words from the text data. Default is FALSE.
+#'
+#' @return A data frame with sentiment scores and labels for each method and overall majority sentiment labels for
+#'         each text column per row.
 #'
 #' @import tidyverse
 #' @import tidyr
@@ -28,7 +35,8 @@
 #'   sample_data,
 #'   text_columns = c("product", "service", "loyalty", "recommend"),
 #'   methods = c("syuzhet", "afinn", "bing"),
-#'   preprocess = TRUE
+#'   preprocess = TRUE,
+#'   remove_stopwords = TRUE
 #' )
 #'
 #' # View the resulting data frame
@@ -43,17 +51,26 @@ library(tidyr)
 library(tidytext)
 library(ggplot2)
 library(syuzhet)
+library(tm)
 
 # Function to analyze sentiments
-analyze_sentiments <- function(data, text_columns, methods = c("syuzhet", "afinn", "bing"), preprocess = FALSE) {
+analyze_sentiments <- function(data, text_columns, methods = c("syuzhet", "afinn", "bing"), preprocess = FALSE, remove_stopwords = FALSE) {
   afinn <- get_sentiments("afinn")
   lexicon_bing <- get_sentiments("bing")
+  stop_words <- get_stopwords()
   
-  if (preprocess) {
+  if (preprocess || remove_stopwords) {
     for (col_name in text_columns) {
-      data[[col_name]] <- str_to_lower(data[[col_name]])
-      data[[col_name]] <- str_replace_all(data[[col_name]], "[^[:alnum:][:space:]]", " ")
-      data[[col_name]] <- str_squish(data[[col_name]])
+      if (preprocess) {
+        data[[col_name]] <- str_to_lower(data[[col_name]])
+        data[[col_name]] <- str_replace_all(data[[col_name]], "[^[:alnum:][:space:]]", " ")
+        data[[col_name]] <- str_squish(data[[col_name]])
+      }
+      
+      if (remove_stopwords) {
+        data <- data %>%
+          mutate(!!col_name := removeWords(!!sym(col_name), stop_words$word))
+      }
     }
   }
   
@@ -64,21 +81,25 @@ analyze_sentiments <- function(data, text_columns, methods = c("syuzhet", "afinn
       df_tidy <- data %>% unnest_tokens(word, !!sym(col_name))
       
       if (method == "syuzhet") {
-        df_tidy <- df_tidy %>% mutate(sentiment = get_sentiment(word))
+        df_tidy <- df_tidy %>% mutate(sentiment = as.numeric(get_sentiment(word)))
       } else if (method == "afinn") {
-        df_tidy <- df_tidy %>% left_join(afinn, by = "word") %>% replace_na(list(value = 0)) %>% mutate(sentiment = value) %>% select(-value)
+        df_tidy <- df_tidy %>% left_join(afinn, by = "word") %>% replace_na(list(value = 0)) %>% mutate(sentiment = as.numeric(value)) %>% select(-value)
       } else if (method == "bing") {
-        df_tidy <- df_tidy %>% left_join(lexicon_bing, by = "word") %>% replace_na(list(sentiment = "neutral")) %>% mutate(sentiment = if_else(sentiment == "positive", 1, if_else(sentiment == "negative", -1, 0)))
+        df_tidy <- df_tidy %>% left_join(lexicon_bing, by = "word") %>% replace_na(list(sentiment = "neutral")) %>% mutate(sentiment = as.numeric(if_else(sentiment == "positive", 1, if_else(sentiment == "negative", -1, 0))))
       }
       
-      df_tidy_sum <- df_tidy %>% group_by(rn) %>% summarize(sentiment_sum = sum(sentiment), .groups = "drop")
+      df_tidy_sum <- df_tidy %>% group_by(rn) %>% summarize(sentiment_sum = sum(sentiment, na.rm = TRUE), .groups = "drop")
       
       data <- left_join(data, df_tidy_sum, by = "rn") %>% rename_with(~ paste0(col_name, "_", method, "_score"), .cols = "sentiment_sum")
       
-      # Add sentiment labels for each cell
+      # Add sentiment labels for each cell using method-specific thresholds
       data <- data %>% mutate(!!paste0(col_name, "_", method, "_label") := case_when(
-        !!sym(paste0(col_name, "_", method, "_score")) > 0 ~ "positive",
-        !!sym(paste0(col_name, "_", method, "_score")) < 0 ~ "negative",
+        method == "syuzhet" & !!sym(paste0(col_name, "_", method, "_score")) > 0.5 ~ "positive",
+        method == "syuzhet" & !!sym(paste0(col_name, "_", method, "_score")) < -0.5 ~ "negative",
+        method == "afinn" & !!sym(paste0(col_name, "_", method, "_score")) > 5 ~ "positive",
+        method == "afinn" & !!sym(paste0(col_name, "_", method, "_score")) < -5 ~ "negative",
+        method == "bing" & !!sym(paste0(col_name, "_", method, "_score")) > 1 ~ "positive",
+        method == "bing" & !!sym(paste0(col_name, "_", method, "_score")) < -1 ~ "negative",
         TRUE ~ "neutral"
       ))
     }
@@ -91,6 +112,5 @@ analyze_sentiments <- function(data, text_columns, methods = c("syuzhet", "afinn
     ))
   }
   
-  data <- data %>% select(-rn)
   return(data)
 }
