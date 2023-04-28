1617cec712a7ab893c97f0cbf31756deef4909d5
elkalytics <napoleonic_bores@proton.me>
2023-04-28 18:45:22 -0400

    Updated functions
    Added documentation as well as other helper functions

--- get_stats.R
+++ get_stats.R
@@ -1,4 +1,265 @@
-#' get_stats
+#' Table of Contents and Description
+#' 
+#' # LINE 036 - get_date_stats() - span, invalid, middle point, average distance, longest streak, shortest streak, max days between, minimum days between
+#' # LINE 075 - get_factor_stats() - level counts, observation counts, observation percents, mode
+#' # LINE 104 - get_gis_stats() - centroid, distance, proximity, convex hull, greatest distance
+#' # LINE 149 - get_multi_stats() - nulls, percent not null, unique levels, highest value, lowest value, average length, max length, minimum length
+#' # LINE 234 - get_numeric_stats() - mean, median, mode, sample standard deviation, population standard deviation, kurtosis, skewness, sum, absolute sum
+#' # LINE 311 - get_numeric_stats() - mean, median, mode, sample standard deviation, population standard deviation, kurtosis, skewness, sum, absolute sum, minimum, max, number of nulls, percent of data not null (allows for grouping)
+#'
+#'
+# Load library
+library(moments)
+#' Get statistics for dates
+#'
+#' This function takes a column of dates and returns statistics regarding the distribution of dates.
+#'
+#' @param column A vector of dates. 
+#' @return A list containing the following statistics:
+#' \describe{
+#'   \item{span}{The number of days between the earliest and latest dates.}
+#'   \item{invalid}{The number of missing values in the input column.}
+#'   \item{middle_date}{The date that falls in the middle of the input date range.}
+#'   \item{avg_dist}{The average number of days between consecutive dates.}
+#'   \item{longest_streak}{The longest consecutive streak of days in the input column.}
+#'   \item{shortest_streak}{The shortest consecutive streak of days in the input column.}
+#'   \item{max_days_between}{The maximum number of days between consecutive dates in the input column.}
+#'   \item{min_days_between}{The minimum number of days between consecutive dates in the input column.}
+#' }
+#'
+#' @importFrom base is.vector as.vector mean sum diff min max sort unique rle is.na
+#' @examples
+#' column <- as.Date(c("2020-01-01", "2020-01-02", "2020-01-05"))
+#' get_date_stats(column)
+#' @export
+# Get statistics for dates
+get_date_stats <- function(column) {
+  if (!is.vector(column)) {
+    column <- as.vector(column)
+  }
+  
+  span <- max(column, na.rm = TRUE) - min(column, na.rm = TRUE)
+  invalid <- sum(is.na(column))
+  middle_date <- min(column, na.rm = TRUE) + 0.5 * span
+  avg_dist <- mean(diff(sort(unique(column[!is.na(column)]))))
+  streaks <- rle(diff(column))
+  longest_streak <- max(streaks$lengths)
+  shortest_streak <- min(streaks$lengths)
+  max_days_between <- max(diff(column), na.rm = TRUE)
+  min_days_between <- min(diff(column), na.rm = TRUE)
+  
+  return(list(span = span, invalid = invalid, middle_date = middle_date,
+              avg_dist = avg_dist, longest_streak = longest_streak, shortest_streak = shortest_streak,
+              max_days_between = max_days_between, min_days_between = min_days_between))
+}
+
+
+#' Get statistics for factors
+#'
+#' This function takes a column of factors and returns statistics regarding the distribution of factor levels.
+#'
+#' @param column A vector of factors. 
+#' @return A list containing the following statistics:
+#' \describe{
+#'   \item{levels_count}{The number of unique levels in the input factor.}
+#'   \item{obs_count}{A table of the frequency counts of each factor level.}
+#'   \item{obs_perc}{The percentage of the total number of observations that each factor level represents.}
+#'   \item{mode}{The most frequently occurring factor level.}
+#' }
+#'
+#' @importFrom base length levels table round prop.table which.max
+#' @examples
+#' column <- factor(c("A", "B", "B", "C"))
+#' get_factor_stats(column)
+#' @export
+get_factor_stats <- function(column) {
+  levels_count <- length(levels(column))
+  obs_count <- table(column)
+  obs_perc <- round(100 * prop.table(obs_count), 2)
+  mode_val <- levels(column)[which.max(obs_count)]
+  
+  return(list(levels_count = levels_count, obs_count = obs_count,
+              obs_perc = obs_perc, mode = mode_val))
+}
+
+
+#' Get statistics for geographic coordinates
+#'
+#' This function takes a data frame with longitude and latitude columns, and returns various statistics related to the coordinates.
+#'
+#' @param column A data frame with longitude and latitude columns. 
+#' @return A list containing the following statistics:
+#' \describe{
+#'   \item{centroid}{The centroid of the coordinates.}
+#'   \item{distance}{A matrix of distances between the coordinates.}
+#'   \item{proximity}{A matrix of the two nearest points for each coordinate.}
+#'   \item{convex_hull}{The convex hull of the coordinates.}
+#'   \item{greatest_distance}{The greatest distance between any two coordinates.}
+#' }
+#'
+#' @importFrom sf st_as_sf st_centroid st_distance st_nearest_points st_convex_hull
+#' @examples
+#' coords <- data.frame(longitude = c(-122.4194, -118.2439, -122.0574), latitude = c(37.7749, 34.0522, 37.3854))
+#' get_gis_stats(coords)
+get_gis_stats <- function(column) {
+  coords <- c("longitude", "latitude")
+  if (is.data.frame(column) && all(tolower(colnames(column)) %in% coords)) {
+    coordinates <- sf::st_as_sf(column, coords = coords, crs = 4326)
+    centroid <- sf::st_centroid(coordinates)
+    distance_matrix <- sf::st_distance(coordinates)
+    proximity <- sf::st_nearest_points(coordinates, coordinates) 
+    convex_hull <- sf::st_convex_hull(coordinates)
+    greatest_distance <- max(as.vector(distance_matrix))
+    
+    return(list(centroid = centroid, distance = distance_matrix,
+                proximity = proximity, convex_hull = convex_hull,
+                greatest_distance = greatest_distance))
+  } else {
+    return(NA)
+  }
+}
+
+
+#' Get statistics for multiple types of variables
+#'
+#' This function takes a data frame and returns basic statistics for each column.
+#'
+#' @param data A data frame with columns of various types. 
+#' @return A data frame containing the following statistics for each column:
+#' \describe{
+#'   \item{column}{The name of the column.}
+#'   \item{nulls}{The number of null values in the column.}
+#'   \item{pct_non_null}{The percentage of non-null values in the column.}
+#'   \item{unique_levels}{The number of unique levels in the column.}
+#'   \item{highest_value}{The highest value in the column.}
+#'   \item{lowest_value}{The lowest value in the column.}
+#'   \item{avg_length}{The average length of values in the column.}
+#'   \item{max_length}{The maximum length of values in the column.}
+#'   \item{min_length}{The minimum length of values in the column.}
+#' }
+#'
+#' @importFrom base sum is.na length unique as.character nchar min max
+#' @importFrom base typeof data.frame
+#' @examples
+#' data <- data.frame(x = c(1, 2, 3, 1, 5),
+#'                    y = c("foo", "bar", "foo", "baz", "haq"),
+#'                    z = factor(c("a", "b", "c", "a", "b")))
+#' get_multi_stats(data)
+#' @export
+get_multi_stats <- function(data) {
+  
+  # initialize an empty data frame to store the results
+  results <- data.frame(column = character(), 
+                        nulls = numeric(),
+                        pct_non_null = numeric(),
+                        unique_levels = numeric(),
+                        highest_value = character(),
+                        lowest_value = character(),
+                        avg_length = numeric(),
+                        max_length = numeric(),
+                        min_length = numeric(),
+                        stringsAsFactors = FALSE)  # add this element to prevent character columns from being converted to factors
+  
+  for (i in 1:ncol(data)) {
+    
+    col_name <- names(data)[i]
+    col_data <- data[[i]]
+    col_type <- typeof(col_data)
+    
+    # calculate the number of nulls and percentage non-null
+    null_count <- sum(is.na(col_data))
+    total_count <- length(col_data)
+    pct_non_null <- 100 - (null_count / total_count) * 100
+    
+    # calculate the number of unique levels
+    unique_levels <- length(unique(col_data))
+    
+    # calculate the highest and lowest values
+    if (is.numeric(col_data)) {
+      highest_value <- max(col_data, na.rm = TRUE)
+      lowest_value <- min(col_data, na.rm = TRUE)
+    } else if (is.character(col_data)) {
+      highest_value <- max(col_data, na.rm = TRUE)
+      lowest_value <- min(col_data, na.rm = TRUE)
+    } else if (is.factor(col_data)) {
+      highest_value <- levels(col_data)[length(levels(col_data))]
+      lowest_value <- levels(col_data)[1]
+    } else {
+      highest_value <- NA
+      lowest_value <- NA
+    }
+    
+    # calculate the average, max, and min lengths
+    avg_length <- mean(nchar(as.character(col_data)), na.rm = TRUE)
+    max_length <- max(nchar(as.character(col_data)), na.rm = TRUE)
+    min_length <- min(nchar(as.character(col_data)), na.rm = TRUE)
+    
+    # add the results to the data frame
+    results[i,] <- c(col_name, null_count, pct_non_null, unique_levels, highest_value, lowest_value, 
+                     avg_length, max_length, min_length)
+  }
+  
+  # reorder the columns in the data frame to match the order in the function definition
+  results <- results[, c("column", "nulls", "pct_non_null", "unique_levels", "highest_value", "lowest_value", "avg_length", "max_length", "min_length")]
+  
+  # return the results data frame
+  return(results)
+}
+
+
+#' Get statistics for numerics
+#'
+#' This function takes a column of numeric values and returns basic statistics for the column.
+#'
+#' @param column A vector of numeric values. 
+#' @return A vector containing the following statistics:
+#' \describe{
+#'   \item{mean_val}{The mean of the input numeric values.}
+#'   \item{median_val}{The median of the input numeric values.}
+#'   \item{mode_val}{The mode of the input numeric values.}
+#'   \item{sample_sd}{The sample standard deviation of the input numeric values.}
+#'   \item{pop_sd}{The population standard deviation of the input numeric values.}
+#'   \item{kurtosis_val}{The kurtosis of the input numeric values.}
+#'   \item{skewness_val}{The skewness of the input numeric values.}
+#'   \item{sum_val}{The sum of the input numeric values.}
+#'   \item{abs_sum_val}{The sum of the absolute values of the input numeric values.}
+#' }
+#'
+#' @importFrom base mean median is.na sd sum abs
+#' @importFrom moments kurtosis skewness
+#' @examples
+#' column <- c(1, 2, 3, 4, 5, NA)
+#' get_numeric_stats(column)
+#' @export
+get_numeric_stats <- function(column) {
+  mean_val <- if (all(is.na(column))) {
+    NA
+  } else {
+    mean(column, na.rm = TRUE)
+  }
+  
+  median_val <- if (all(is.na(column))) {
+    NA
+  } else {
+    median(column, na.rm = TRUE)
+  }
+  mode_val <- if (length(column[!is.na(column)]) > 0) {
+    as.numeric(names(sort(-table(column), decreasing = TRUE)))[1]
+  } else {
+    NA
+  }
+  sample_sd <- sd(column, na.rm = TRUE) / sqrt(length(column) - 1)
+  pop_sd <- sd(column, na.rm = TRUE)
+  kurtosis_val <- kurtosis(column, na.rm = TRUE)
+  skewness_val <- skewness(column, na.rm = TRUE)
+  sum_val <- sum(column, na.rm = TRUE)
+  abs_sum_val <- sum(abs(column), na.rm = TRUE)
+  
+  return(c(mean_val, median_val, mode_val, sample_sd, pop_sd, kurtosis_val, skewness_val, sum_val, abs_sum_val))
+}
+
+
+#' get_science_stats
 #'
 #' This function calculates various summary statistics of a data frame or its subsets based on a grouping variable.
 #'
@@ -41,15 +302,13 @@
 #' @examples
 #' # Calculate statistics for a whole data frame
 #' data(mtcars)
-#' get_stats(mtcars) -> mtcars_stats
+#' get_science_stats(mtcars) -> mtcars_stats
 #'
 #' # Calculate statistics for subsets of a data frame
 #' data(mtcars)
-#' get_stats(mtcars, group = "cyl") -> mtcars_stats_group_cycle
-# Load library
-library(moments)
-# Save function
-get_stats <- function(df, group = NULL) {
+#' get_science_stats(mtcars, group = "cyl") -> mtcars_stats_group_cycle
+#' @export
+get_science_stats <- function(df, group = NULL) {
   
   if (is.null(group)) {
     result <- data.frame(matrix(ncol = 11, nrow = ncol(df)))
