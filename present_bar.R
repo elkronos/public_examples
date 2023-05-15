d654c26409b1e220abf6daae1e1bd90a6a9940c7
elkalytics <napoleonic_bores@proton.me>
2023-05-15 00:26:34 -0400

    Added an all
    User can now run bar plots for ALL statistics at once.

--- present_bar.R
+++ present_bar.R
@@ -1,42 +1,64 @@
-#' Create bar plots for combinations of categorical and numeric variables
+#' Create Bar Plots for PowerPoint
 #'
-#' This function generates bar plots for each combination of categorical and numeric variables
-#' in a dataset, using a specified statistic. It creates a PowerPoint presentation with each plot
-#' on a separate slide. The user can customize the colors and other aspects of the plots.
+#' This function generates bar plots of numeric variables grouped by categorical variables
+#' and exports them to a PowerPoint file.
 #'
-#' @param data A data.frame containing the dataset to create bar plots from.
-#' @param statistic A character string specifying the statistic to use for the bar plots
-#'   ("mean", "median", "min", "max", or "sum"). Default is "mean".
-#' @param unique_levels An integer specifying the maximum number of unique levels for
-#'   a variable to be considered categorical. Default is 10.
-#' @param custom_colors A named vector of colors to use for customizing the colors of the bars.
-#'   Default is NULL, which uses the hue_pal() color palette.
+#' @param data A data frame containing the data to be plotted.
+#' @param statistic A character string specifying the statistic to be plotted.
+#'   Options: "all", "mean", "median", "min", "max", "sum", "count". Default: "all".
+#' @param unique_levels An integer specifying the maximum number of unique levels a
+#'   variable can have to be considered categorical. Default: 10.
+#' @param custom_colors A vector of custom colors to use in the plots. Default: NULL.
+#' @param file_name A character string specifying the base name of the output PowerPoint file.
+#'   Default: "bar_plots".
+#' @param custom_theme A ggplot2 theme to apply to the plots. Default: theme_minimal().
+#' @param x_axis_label A character string specifying the label for the x-axis. Default: NULL.
+#' @param y_axis_label A character string specifying the label for the y-axis. Default: NULL.
+#' @param round_digits An integer specifying the number of decimal places to round the values.
+#'   Default: 1.
 #'
-#' @importFrom ggplot2 ggplot aes_string geom_bar geom_text labs theme_minimal theme element_text
+#' @importFrom ggplot2 ggplot aes_string geom_bar geom_text labs theme_minimal element_text
 #' @importFrom officer read_pptx add_slide ph_with print
 #' @importFrom rvg ph_location_type
 #' @importFrom dplyr group_by summarize
-#' @importFrom scales hue_pal
+#' @importFrom scales hue_pal scale_fill_manual
+#' @importFrom tools toTitleCase
 #'
 #' @examples
-#' # Create bar plots with default settings
-#' present_bar(diamonds, statistic = "mean", unique_levels = 10)
+#' \dontrun{
+#'   present_bar(diamonds, statistic = "all", unique_levels = 10)
+#' }
 #'
-#' # Create bar plots with custom colors
-#' custom_colors <- c("Ideal" = "red", "Premium" = "blue", "Good" = "green",
-#'                    "Very Good" = "yellow", "Fair" = "purple")
-#' present_bar(diamonds, statistic = "mean", unique_levels = 10, custom_colors = custom_colors)
-#'
-#' @return A PowerPoint presentation saved as "bar_plots.pptx" in the working directory.
-#' @export
 # Load packages
 library(ggplot2)
 library(officer)
 library(rvg)
 library(dplyr)
 library(scales)
+library(tools)
 # Save function
-present_bar <- function(data, statistic = "mean", unique_levels = 10, custom_colors = NULL) {
+present_bar <- function(data,
+                        statistic = "all",
+                        unique_levels = 10,
+                        custom_colors = NULL,
+                        file_name = "bar_plots",
+                        custom_theme = theme_minimal(),
+                        x_axis_label = NULL,
+                        y_axis_label = NULL,
+                        round_digits = 1) {
+  
+  # Validate the input statistic
+  valid_statistics <- c("all", "mean", "median", "min", "max", "sum", "count")
+  if (!statistic %in% valid_statistics) {
+    stop("Invalid statistic. Choose from 'all', 'mean', 'median', 'min', 'max', 'sum', or 'count'.")
+  }
+  
+  if (statistic == "all") {
+    statistics <- valid_statistics[-1]  # Remove "all" from the list
+  } else {
+    statistics <- statistic
+  }
+  
   # Identify variables with the specified number of unique levels or fewer
   categorical_vars <- sapply(data, function(x) length(unique(x)) <= unique_levels)
   categorical_vars <- names(categorical_vars[categorical_vars])
@@ -44,15 +66,6 @@ present_bar <- function(data, statistic = "mean", unique_levels = 10, custom_col
   # Identify numeric variables
   numeric_vars <- sapply(data, is.numeric)
   numeric_vars <- names(numeric_vars[numeric_vars])
-  
-  # Define the statistic function
-  stat_func <- switch(statistic,
-                      mean = mean,
-                      median = median,
-                      min = min,
-                      max = max,
-                      sum = sum,
-                      stop("Invalid statistic. Choose from mean, median, min, max, or sum."))
   
   # Set color palette
   max_unique_levels <- max(sapply(data[categorical_vars], function(x) length(unique(x))))
@@ -66,30 +79,53 @@ present_bar <- function(data, statistic = "mean", unique_levels = 10, custom_col
   # Create a PowerPoint object
   pptx <- read_pptx()
   
-  # Loop through the selected categorical and numeric variables
-  for (cat_var in categorical_vars) {
-    for (num_var in numeric_vars) {
-      # Generate a bar plot for the current variable
-      plot_data <- data %>%
-        group_by(!!sym(cat_var)) %>%
-        summarize(value = stat_func(!!sym(num_var), na.rm = TRUE))
-      
-      plot <- ggplot(plot_data, aes_string(x = cat_var, y = "value", fill = cat_var)) +
-        geom_bar(stat = "identity", show.legend = FALSE) +
-        geom_text(aes(label = round(value, 1)), vjust = -0.5) +
-        labs(title = paste(statistic, "of", num_var, "by", cat_var),
-             x = cat_var, y = num_var) +
-        theme_minimal() +
-        theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-        color_palette
-      
-      # Add a slide to the PowerPoint object and include the plot
-      pptx <- pptx %>%
-        add_slide(layout = "Title and Content", master = "Office Theme") %>%
-        ph_with(value = plot, location = ph_location_type(type = "body"))
+  # Loop through the selected statistics
+  for (stat in statistics) {
+    
+    # Loop through the selected categorical and numeric variables
+    for (cat_var in categorical_vars) {
+      for (num_var in numeric_vars) {
+        # Generate a bar plot for the current variable
+        if (stat != "count") {
+          stat_func <- switch(stat,
+                              mean = mean,
+                              median = median,
+                              min = min,
+                              max = max,
+                              sum = sum)
+          plot_data <- data %>%
+            group_by(!!sym(cat_var)) %>%
+            summarize(value = stat_func(!!sym(num_var), na.rm = TRUE),
+                      .groups = "drop")
+        } else {
+          plot_data <- data %>%
+            group_by(!!sym(cat_var)) %>%
+            summarize(value = n(),
+                      .groups = "drop")
+        }
+        
+        plot <- ggplot(plot_data, aes_string(x = cat_var, y = "value", fill = cat_var)) +
+          geom_bar(stat = "identity", show.legend = FALSE) +
+          geom_text(aes(label = round(value, round_digits)), vjust = -0.5) +
+          labs(title = toTitleCase(paste(stat, "of", num_var, "by", cat_var)),
+               x = ifelse(is.null(x_axis_label), cat_var, x_axis_label),
+               y = ifelse(is.null(y_axis_label), num_var, y_axis_label)) +
+          custom_theme +
+          theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+          color_palette
+        
+        # Add a slide to the PowerPoint object and include the plot
+        pptx <- pptx %>%
+          add_slide(layout = "Title and Content", master = "Office Theme") %>%
+          ph_with(value = plot, location = ph_location_type(type = "body"))
+      }
     }
   }
   
+  # Add the system date and time to the end of the file name
+  current_datetime <- format(Sys.time(), "%Y%m%d_%H%M%S")
+  file_name <- paste0(file_name, "_", current_datetime, ".pptx")
+  
   # Save the PowerPoint object to a file
-  print(pptx, target = "bar_plots.pptx")
+  print(pptx, target = file_name)
 }
